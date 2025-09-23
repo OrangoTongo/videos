9ca97b6fd68c0319162305f9c5ff5feb4da6f9ba
OrangoTongo <email.para.teste2025@proton.me>
2025-09-23 15:04:49 -0300

    build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Inicio from "./pages/Inicio.jsx";
-import Filmes from "./pages/Filmes.jsx";
-import Series from "./pages/Series.jsx";
+import Home from "./components/Home.jsx";
+import Filmes from "./components/Filmes.jsx";
+import Series from "./components/Series.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Inicio />} />
+        <Route path="/" element={<Home />} />
         <Route path="/filmes" element={<Filmes />} />
         <Route path="/series" element={<Series />} />
       </Routes>
