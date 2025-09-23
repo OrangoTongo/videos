78d52ef248a68c3cbcb20c41cdc10b6c3feebcc3
OrangoTongo <email.para.teste2025@proton.me>
2025-09-23 11:47:19 -0300

    Add files via upload

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,33 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import cors from "cors";
+
+// Routers da API
 import indexRouter from "./routes/indexRouter.js";
 import seriesRouter from "./routes/seriesRouter.js";
 import filmesRouter from "./routes/filmesRouter.js";
 
 const app = express();
-app.use(express.json());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// Rotas
-app.use("/inicio", indexRouter);
+
+app.use(express.json());
+app.use(cors());
+
+
+app.use("//inicio", indexRouter);
 app.use("/series", seriesRouter);
 app.use("/filmes", filmesRouter);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Backend rodando na porta ${PORT}`));
